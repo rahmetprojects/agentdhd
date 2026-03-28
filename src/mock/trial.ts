11a0074f18a365947ce0aa3dc8b17fc1a881700d
rahmetprojects <rahmetasfaw2@gmail.com>
2026-03-28 23:17:57 +0400

    Update trial.ts

--- src/mock/trial.ts
+++ src/mock/trial.ts
@@ -1,4 +1,73 @@
-let age: number = 20;
-age = 23;
-if (age < 30){
-    console.log(false);}
+// Using square brackets
+let numbers: number[] = [1, 2, 3, 4, 5];
+// Using generic Array type
+let fruits: Array<string> = ["Apple", "Banana", "Cherry"];
+// Accessing elements
+console.log(numbers[0]); // 1
+console.log(fruits[2]); // Cherry
+
+let boolArray: boolean[] = [true, false, true];
+let mixedValues: Array<string | number> = ["Apple", 42, "Orange"];
+let matrix: number[][] = [
+   [1, 2, 3],
+   [4, 5, 6]
+];
+console.log(matrix[0]?.[1]); // 6
+
+//tuples
+let user: [number, string] = [1, 'Mosh'];
+//wrong
+user.push(1);
+
+
+function calculateTax(income: number ): number {
+    if (income < 50)
+        return income*1.2;
+    return 0;
+}
+
+//type alias
+type Employee = {
+    readonly id: number, name: string, retire: (date: Date)=>void
+}
+
+let employee: { readonly id: number, name: string, retire: (date: Date)=>void} = 
+{id: 1 ,name:'Mosh', retire: (date: Date)=> {console.log(date)}};
+employee.name = 'Mo'
+
+//let employe: Employee = {
+//    id: 1,...
+//}
+
+function kgtolbs (weight: number | string): number {
+    if (typeof weight === 'number'){
+        return weight*2.2;
+    }
+    else
+        return parseInt(weight) * 2.2;
+}
+
+type Draggable = {
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    //textBox is a UIWidget object so it
+    //needs to implement both
+    drag: () => {},
+    resize: () => {}
+}
+
+
+const person = {
+  firstName: "John",
+  lastName: "Doe"
+};
+
+let result = ("firstName" in person);
