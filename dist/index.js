52e7c69bb3609bda27a32125550e8d2ffc10ac6a
rahmetprojects <rahmetasfaw2@gmail.com>
2026-03-28 22:42:15 +0400

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 20;
+age = 23;
+if (age < 30) {
+    console.log(false);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/mock/trial.ts
@@ -0,0 +1,4 @@
+let age: number = 20;
+age = 23;
+if (age < 30){
+    console.log(false);}
