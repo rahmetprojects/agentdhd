59e0dd6d7ad49dc9404d4b7679e61b2dce6bdb17
rahmetprojects <rahmetasfaw2@gmail.com>
2026-03-28 23:12:17 +0400

    Remove .vscode and dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let age = 20;
-age = 23;
-if (age < 30) {
-    console.log(false);
-}
-//# sourceMappingURL=index.js.map
